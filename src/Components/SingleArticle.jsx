59f61eb4106cda1c11a9cc0d796a4c6713819c01
rallosdrake <jimbobbarlow74@gmail.com>
2022-04-21 12:31:27 +0100

    added style to fullscreen

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -34,7 +34,7 @@ export const SingleArticle = () => {
 
   if (!isLoading && !article) return <p>"Error404"</p>;
   if (err) return <p>{err}</p>;
-  if (isLoading) return <h1> Loading</h1>;
+  if (isLoading) return <h1> Loading...</h1>;
   return (
     <div className="articleBody">
       <h2>{article.title}</h2>
